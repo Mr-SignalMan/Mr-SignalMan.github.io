6b84788c7286791d998fbaae73e67eac3c8596d1
Oleg <90524766+TikhomirovOleg@users.noreply.github.com>
2021-10-01 15:05:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+	document.getElementById('one').style.display = 'block';
+}, 1500);
+setTimeout(function(){
+	document.getElementById('two').style.display = 'none';
+}, 1500);
